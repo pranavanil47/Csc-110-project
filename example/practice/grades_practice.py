964acd290bcf6eed5b3d21c86d4db69caf1f1223
pranavanil47 <pranavanil1232@gmail.com>
2021-03-08 20:52:57 -0700

    Update grades_practice.py

--- example/practice/grades_practice.py
+++ example/practice/grades_practice.py
@@ -1,19 +1,31 @@
 def get_score():
-    grades_list =[0]
+    grades_list =[]
 
     subjects = int(input('Enter the number of subjects to calculate the average '\
         ))
     
     while subjects>0:
-        scores = int(input('Enter scores for', 'subject: ' ))
-        
+        scores = int(input('Enter scores for subject: ' ))
+        grades_list.append(scores)
         subjects-=1
     
+    return grades_list
 
-    return grades_list ,scores
+def kick_out_min():
+    g= get_score()
+    minimum_score = min(g)
+    g.remove(minimum_score)
+    return g
+
+def calculate_average():
+    grades = kick_out_min()
+    count = 0
+    for i in range(0,len(grades)-1):
+        count+= grades[i]
+    return count
 
 def main():
-    print(get_score())
+    print(calculate_average)
     
 
 main()
